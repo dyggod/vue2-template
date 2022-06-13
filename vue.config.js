b9da837120f2c2ac390a64dd4ae070f7cb5e2202
Yinggang.Dong <dyggod@163.com>
2022-06-13 14:25:35 +0800

    feat: 引入antd-design-vue@1.7.8，配置less

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
 const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [path.resolve(__dirname, './src/theme/theme.less')],
+    },
+  },
+  css: {
+    loaderOptions: {
+      less: {
+        // 配置less选项
+        lessOptions: {
+          javascriptEnabled: true,
+        },
+      },
+    },
+  },
 });
